1653b1a4c307d8a4731e13f9cf442c1637a0d8a4
wisdomCX <1361791276@qq.com>
2021-02-22 21:25:35 +0800

    触摸方式

--- app/card.vue
+++ app/card.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="topDiv" align="center">
-    <div @dragover="dragOver(0)">
-      <div width="5%" @dragenter="currentTop(0, 0)">
+    <div :hidden="moveStat" width="10%" :style="{ left: mLeft, top: mTop }">
+      <img src="./card.img/back.png" width="10%" />
+    </div>
+    <div @dragover="dragOver(0)" class="moveStat">
+      <div width="5%" @dragenter="currentTop(0, 0)"  class="c0-0" >
         <img src="./card.img/side.png" width="90%" />
       </div>
 
@@ -13,15 +16,18 @@
           @dragstart="currentTop(index, 0)"
           @dragend="aTdrop(index, 0)"
           width="80%"
+          @touchstart="touchStart(index, 0)"
+          @touchmove="touchMove($event, val)"
+          @touchend="touchEnd(index,0,$event, val)"
         />
       </div>
-      <div width="5%" @dragenter="currentTop(cardList[0].length - 1, 0)">
+      <div width="5%" class="c1-0"  @dragenter="currentTop(cardList[0].length - 1, 0)">
         <img src="./card.img/side.png" width="90%" />
       </div>
     </div>
 
     <div :dragover="dragOver(1)">
-      <div width="5%" @dragenter="currentTop(0, 1)">
+      <div width="5%" class="c0-1" @dragenter="currentTop(0, 1)">
         <img src="./card.img/side.png" width="90%" />
       </div>
 
@@ -32,10 +38,17 @@
           @dragend="aTdrop(index, 1)"
           @dragstart="currentTop(index, 1)"
           width="80%"
+          @touchstart="touchStart(index, 1)"
+          @touchmove="touchMove($event, val)"
+          @touchend="touchEnd(index,1,$event, val)"
         />
       </div>
 
-      <div width="5%" @dragenter="currentTop(cardList[1].length - 1, 1)">
+      <div class="c1-1"
+        width="5%"
+        @dragenter="currentTop(cardList[1].length - 1, 1)"
+        @dragover="atdragover"
+      >
         <img src="./card.img/side.png" width="90%" />
       </div>
     </div>
@@ -51,14 +64,14 @@
 </template>
 
 <script>
-var rq = require("arrayHelp/rand-lib/rand-uq");
-var { compareFull } = require("arrayHelp/array.compare/index");
-export default {
+var rq = require("arrayhelp/rand-lib/rand-uq");
+var { compareFull } = require("arrayhelp/array.compare/index");
+module.exports = {
   data() {
-    return {
+    var obj= {
       cardList: [
         [1, 2, 3],
-        [0, 1, 2],
+        [1, 2, 3],
       ],
       mousePoint: {
         index: 0,
@@ -75,7 +88,13 @@ export default {
       win: false,
       mtime: 0,
       modStat: 1,
+      moveStat: false,
+      mLeft: 50,
+      mTop: 50,
+      // mWidth"10%",
     };
+	// this.init2();
+  return obj
   },
   methods: {
     getSrc: function (val, index, arr) {
@@ -85,16 +104,12 @@ export default {
       // }else{
       // 	return  './card.img/back.png'
       // }
+      console.log(cardNum);
+      if (val == 0) debugger;
       var cardNum = val.toString().padStart(4, "0");
       return `./card.img/${cardNum}.png`;
     },
     currentTop(index, row) {
-      // if(this.index==-1&&this.row==-1){
-      // 	setTimeout(()=>{
-      // this.mousePoint.index = index;
-      // this.mousePoint.row = row;
-      // 	},700)
-      // }
       this.mousePoint.index = index;
       this.mousePoint.row = row;
       console.log("目标", this.mousePoint.row, this.mousePoint.index);
@@ -136,6 +151,62 @@ export default {
       this.$set(this.cardList, "0", rs.splice(0, 13));
       this.$set(this.cardList, "1", rs);
     },
+    touchStart(index, row, evt) {
+      if (index != this.cardList[row].length - 1 && index !== 0) {
+        // ev.preventDefault();
+        return;
+      }
+      console.log(evt);
+      console.log("被拖动了");
+      //  oL = touch.clientX - div1.offsetLeft;
+      // oT = touch.clientY - div1.offsetTop;
+    },
+    touchMove(e,val) {
+      var ev = e;
+      var touch = ev.targetTouches[0];
+      var oLeft = touch.clientX;
+      var oTop = touch.clientY;
+      document.body.offsetWidth;
+
+     /*  if (oLeft < 0) {
+        oLeft = 0;
+      } else if (oLeft >= document.body.offsetWidth) {
+        oLeft = document.body.offsetWidth;
+      }
+      if (oTop < 0) {
+        oTop = 0;
+      } else if (oTop >= document.body.offsetHeight) {
+        oTop = offsetHeight;
+      } */
+      this.mLeft = oLeft + "px";
+      this.mTop = oTop + "px";
+    },
+	touchEnd(index,row,e,val){
+		var element = document.elementFromPoint(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+		console.log();
+    var classStr=element.parentElement.getAttribute("class");
+    if(classStr==""||!/c[1|0]{1}\-{1}[1|0]{1}/.test(classStr)){
+      return
+    }
+    // console.log
+    // var 
+
+    
+    this.mousePoint.row=Number( classStr[3]);
+     this.mousePoint.index =Number( classStr[1]);
+     if(Number( classStr[1])==1){
+        this.mousePoint.index= this.cardList[this.mousePoint.row].length-1
+     }
+    this.aTdrop(index,row);
+    // console.loh("当前牌是",val)
+    // this/
+	  // var dom= this.$createElement(element)
+    // dom.$emit(dragend)
+
+	},
+    atdragover() {
+      console.log(arguments);
+    },
   },
   mounted() {
     if (this.modStat == 1) {
@@ -144,9 +215,15 @@ export default {
     globalThis.vv = this;
   },
 };
+// module.exports={default:defaul}
 </script>
 
 <style>
+moveStat {
+  width: 10%;
+  z-index: 90;
+  position: absolute;
+}
 .topDiv {
   width: 100%;
   float: center;
